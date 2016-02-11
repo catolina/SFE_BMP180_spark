7a2de0a3979fe118d8d536bcc1a1f77045346538
catolina <k.m.baranowska93@gmail.com>
2016-02-11 20:22:38 +0100

    Update BMP180.h

--- firmware/BMP180.h
+++ firmware/BMP180.h
@@ -20,9 +20,17 @@
 #define SFE_BMP180_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
+#if defined (SPARK)
+  #include "application.h"
 #else
-#include "WProgram.h"
+  #if defined(ARDUINO) && ARDUINO >= 100
+    #include "Arduino.h"
+  #else
+    #include "WProgram.h"
+  #endif
+  // here could follow some of the includes only needed on Arduino
+  // see bellow
+
 #endif
 
 class SFE_BMP180
